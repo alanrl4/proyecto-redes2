a9e8990721b7b2bbfa95d6403b6dbd1fcd272e9f
Alan RL <alan.r.l.4018@gmail.com>
2022-06-09 00:09:26 -0500

    Initial commit

---
+++ client.py
@@ -0,0 +1,87 @@
+from base64 import encode
+from math import ceil
+import socket
+import sys
+import time
+import os
+
+PORTS = (8083, 8084, 8085)
+IPS = ('localhost', 'localhost', 'localhost')
+
+PORT = 8083
+IP = 'localhost'
+SEPARATOR = '<separator>'
+BUFFER_SIZE = 500
+CLIENT_FOLDER = "client_files"
+
+if (len(sys.argv) < 3):
+    print("Missing arguments: client.py OPERATION FILENAME")
+    exit()
+
+try:
+    os.mkdir(CLIENT_FOLDER)
+except:
+    pass
+
+filename = sys.argv[2]
+operation = sys.argv[1]
+conns = (
+    socket.socket(socket.AF_INET, socket.SOCK_STREAM),
+    socket.socket(socket.AF_INET, socket.SOCK_STREAM),
+    socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+)
+
+server_address = (IP, PORT)
+for i in range(3):
+    ip = IPS[i]
+    port = PORTS[i]
+    conns[i].connect((IPS[i], PORTS[i]))
+    print(f'Server {ip}:{port} connected')
+
+try:
+
+    if operation == "upload":
+
+        filesize = os.path.getsize(filename)
+        chunk_size = ceil(filesize/3)
+
+        with open(filename, 'rb') as f:
+
+            for i in range(3):
+                bytes_sent = 0
+
+                conns[i].send(SEPARATOR.join((filename, operation)).encode())
+                if conns[i].recv(BUFFER_SIZE) != b'ACK':
+                    exit()
+                
+                while True:
+                    cur_chunk_size = BUFFER_SIZE;
+                    left_bytes = chunk_size - bytes_sent
+                    if BUFFER_SIZE > left_bytes:
+                        cur_chunk_size = left_bytes
+                    chunk = f.read(cur_chunk_size)
+                    if not chunk:
+                        break
+                    conns[i].sendall(chunk)
+                    bytes_sent += cur_chunk_size
+                    if chunk_size - bytes_sent <= 0:
+                        break
+            print("done")
+    else:
+        n_filename = f'./{CLIENT_FOLDER}/{os.path.basename(filename)}'
+        with open(n_filename, 'wb') as f:
+            for i in range(3):
+                conns[i].send(SEPARATOR.join((filename, operation)).encode())
+                if conns[i].recv(BUFFER_SIZE) == b'1':
+                    while True:
+                        chunk = conns[i].recv(BUFFER_SIZE)
+                        if not chunk:
+                            break
+                        f.write(chunk)
+                else:
+                    print("File not found")
+                    exit()
+        print(n_filename)
+finally:
+    for i in range(i):
+        conns[i].close()
---
+++ server.py
@@ -0,0 +1,63 @@
+
+import socket
+import sys
+import os
+import threading
+
+
+IP = 'localhost'
+SEPARATOR = '<separator>'
+BUFFER_SIZE = 500
+
+if (len(sys.argv) < 3):
+    print("Missing arguments: server.py PORT FOLDER")
+    exit()
+
+PORT = int(sys.argv[1])
+FILES_FOLDER = sys.argv[2]
+try:
+    os.mkdir(FILES_FOLDER)
+except:
+    pass
+
+def handler(conn: socket.socket, client_address: tuple):
+    try:
+        
+        d = conn.recv(BUFFER_SIZE).decode()
+        filename, operation = d.split(SEPARATOR)
+
+        filename = f'./{FILES_FOLDER}/{os.path.basename(filename)}.chunk'
+
+        if operation == "upload":
+            conn.send(b'ACK')
+            with open(filename, 'wb') as f:
+                while True:
+                    d = conn.recv(BUFFER_SIZE)
+                    if not d:
+                        break;
+                    f.write(d)
+        else:
+            if os.path.exists(filename):
+                conn.send(b'1')
+                with open(filename, 'rb') as f:
+                    while True:
+                        chunk = f.read(BUFFER_SIZE)
+                        if not chunk:
+                            break
+                        conn.sendall(chunk)
+                print(f'${filename} sent')
+            else:
+                conn.send(b'0')
+                print(f'${filename} not found')
+    finally:
+        conn.close()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (IP, PORT)
+sock.bind(server_address)
+sock.listen(10);
+
+print('Server listen on {}:{}'.format(*server_address))
+while True:
+    conn, client_address = sock.accept()
+    threading.Thread(target=handler, args=(conn, client_address)).start()
